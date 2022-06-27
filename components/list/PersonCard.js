f8a1c3a0b52215f85b32eb1d94123f2a47ac4267
aabdelw1 <ammar.s.nasr@gmail.com>
2022-06-27 14:42:33 -0500

    Added duration parser

--- components/list/PersonCard.js
+++ components/list/PersonCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import styled from "styled-components/native";
+import { getDuration } from "./hooks/getAgeDifference";
 
 const Container = styled.View`
   display: flex;
-  margin: 15px 10px 0;  
-  /* border-bottom: 1px solid #dddddd; */
+  /* margin: 0px 10px 15px 0 ;   */
+  borderBottomWidth: 1px;
+  padding: 10px;
 `;
 
 const Avatar = styled.View`
@@ -18,23 +20,37 @@ const Avatar = styled.View`
 	margin-right:10px;
 `
 
-const Description = styled.Text`
+const Name = styled.Text`
   font-size: 18px;
-  line-height:30px;
+  font-weight:500;
+  /* line-height:30px; */
+  /* color:${props => props.color}; */
+`
+
+const SubText = styled.Text`
+  font-size: 13px;
+  line-height: 20px;
+  color: #757575;
   /* color:${props => props.color}; */
 `
 
 
 const PersonCard = (props) => {
   const { person } = props;
-  const {  name, meetingOrigin, talkingTime, physicalAltercations, timeLine } = person;
+  const { name, meetingOrigin, talkingTime, physicalAltercations, timeLine } = person;
+
+  const calculateLength = () => {
+    return getDuration(timeLine && timeLine[0])
+  }    
+
   const firstName = name.split(" ")[0]
   return (
     <Container>
       {/* <Avatar>	 */}
-						<Image src={require(`../../assets/navIcons/list.png`)} name={name}/>
+						{/* <Image source={require(`../../assets/navIcons/profile.png`)} name={name}/> */}
 			{/* </Avatar> */}
-      <Description>{meetingOrigin}</Description>
+      <Name>{name}</Name>
+      <SubText>{calculateLength()}</SubText>
     </Container>
   );
 };
